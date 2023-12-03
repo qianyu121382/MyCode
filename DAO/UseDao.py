f574dca6d22253897b3474ab5a7535e9e5dbd3ec
qianyu <2142544864.com>
2023-12-03 14:24:13 +0800

    parent

---
+++ DAO/UseDao.py
@@ -0,0 +1,6 @@
+
+class UseDao:
+    def __init__(self):
+        serverName = '127.0.0.1'
+        pass
+
---
+++ DAO/UseDaoImpl.py
@@ -0,0 +1,9 @@
+from DAO.UseDao import UseDao
+
+
+class UseDaoImpl (UseDao):
+    def __init__(self):
+        super.__init__()
+
+    def
+
